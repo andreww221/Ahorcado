b9837756be266bd4fec50607b20f67add5a931ee
andreww221 <andrewbuitrago221@gmail.com>
2022-06-07 20:36:33 -0500

    Funcionalidad de escribir una letra y dibujarlas en las lineas

--- js/index.js
+++ js/index.js
@@ -1,16 +1,57 @@
-
 import { crearLineass } from "../js/crearLineas.js";
 import { palabraAleatoria } from "../js/palabraAleatoria.js";
 
+var frutas = ["fresa", "papaya", "mango", "manzana"];
+
+const frutaAleatoria = palabraAleatoria(frutas);
+
+console.log(frutaAleatoria);
+
+const inputLetra = document.querySelector("[data-letra]");
+
+const botonLetra = document.querySelector("[data-letraBoton]");
+
+var letraPresionada = "";
+
+function iniciarJuego() {
+  crearLineass(frutaAleatoria);
+}
+
+iniciarJuego();
+
+botonLetra.addEventListener("click", () => {
+  letraPresionada = inputLetra.value;
+
+  console.log(letraPresionada);
+
+  var posiciones = [];
+
+  for (var i = 0; i < frutaAleatoria.length; i++) {
+    if (frutaAleatoria[i] == letraPresionada) {
+      posiciones.push(i);
+    }
+  }
 
 
-var frutas = ["fresa","papaya","mango","manzana"];
-
-crearLineass(palabraAleatoria(frutas));
 
 
+  const lineas = document.querySelectorAll(".raya");
+
+  lineas.forEach((v, i, a) => {
+
+    posiciones.forEach((v1, i1, a1) => {
+
+      if(v1==i){
+
+        v.value=letraPresionada;
 
 
+      }
+
+    });
 
 
+  });
 
+  console.log(lineas);
+});
